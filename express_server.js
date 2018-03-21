17b6b5c2745f43206af6b3d057384a72f6b5d967
Brian Holden <bkavuh@gmail.com>
2018-03-21 12:46:16 +0000

    Delete function added, fixed error in partials

--- express_server.js
+++ express_server.js
@@ -26,17 +26,18 @@ app.get("/", (req, res) => {
   res.end("Hello!");
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  var longURL = '';
+  for (var key in urlDatabase) {
+    if (key === req.params.shortURL){
+      longURL = urlDatabase[key];
+    }
+  }
+  res.redirect(longURL);
+});
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
-});
-
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
 });
 
 app.get("/urls", (req, res) => {
@@ -44,13 +45,26 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  // console.log(req.body.longURL);  // debug statement to see POST parameters
+  var newShort = generateRandomString();
+  urlDatabase[newShort] = req.body.longURL;
+  res.send(req.body.longURL + " Shortend too: " + newShort);         // Respond with 'Ok' (we will replace this)
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("http://localhost:8080/urls");
+});
+
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id, urls: urlDatabase};
   res.render("urls_show", templateVars);
-});
-
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
